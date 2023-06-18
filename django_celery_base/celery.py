c06e487ca7cd6999e7a03577d0a0850607582192
Ankit Sharma <nez.dev@icloud.com>
2023-06-18 18:24:34 +0530

    init and celery configured

---
+++ django_celery_base/celery.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_base.settings')
+
+app = Celery('django_celery_base') # project name
+
+app.conf.enable_utc = False
+app.conf.update(timezone = 'Asia/Kolkata')
+
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object(settings, namespace='Celery')
+
+# Celery beat settings
+#
+
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
